7a34f14ac674de11479c5612ae3671ab136ff7cf
mikezila <matthew.lawson@outlook.com>
2014-01-29 13:38:51 -0500

    initial commit
    First push of code.  Already mostly completed.

---
+++ EasyTLC/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace EasyTLC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tlcBrowser = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BBYuserName = new System.Windows.Forms.TextBox();
+            this.BBYpassWord = new System.Windows.Forms.TextBox();
+            this.scrapeButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.scheduleReadout = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.exportButton = new System.Windows.Forms.Button();
+            this.tlcTimeShift = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tlcTimeShift)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tlcBrowser
+            // 
+            this.tlcBrowser.Location = new System.Drawing.Point(248, 12);
+            this.tlcBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.tlcBrowser.Name = "tlcBrowser";
+            this.tlcBrowser.ScriptErrorsSuppressed = true;
+            this.tlcBrowser.Size = new System.Drawing.Size(856, 534);
+            this.tlcBrowser.TabIndex = 1;
+            this.tlcBrowser.Url = new System.Uri("http://mytlc.bestbuy.com", System.UriKind.Absolute);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(167, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 63);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "aNumber / Password";
+            // 
+            // BBYuserName
+            // 
+            this.BBYuserName.Location = new System.Drawing.Point(13, 30);
+            this.BBYuserName.Name = "BBYuserName";
+            this.BBYuserName.Size = new System.Drawing.Size(148, 20);
+            this.BBYuserName.TabIndex = 4;
+            // 
+            // BBYpassWord
+            // 
+            this.BBYpassWord.Location = new System.Drawing.Point(13, 56);
+            this.BBYpassWord.Name = "BBYpassWord";
+            this.BBYpassWord.Size = new System.Drawing.Size(148, 20);
+            this.BBYpassWord.TabIndex = 5;
+            this.BBYpassWord.UseSystemPasswordChar = true;
+            // 
+            // scrapeButton
+            // 
+            this.scrapeButton.Location = new System.Drawing.Point(16, 125);
+            this.scrapeButton.Name = "scrapeButton";
+            this.scrapeButton.Size = new System.Drawing.Size(226, 23);
+            this.scrapeButton.TabIndex = 7;
+            this.scrapeButton.Text = "Scrape Schedule";
+            this.scrapeButton.UseVisualStyleBackColor = true;
+            this.scrapeButton.Click += new System.EventHandler(this.scrapeButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Click once schedule is on-screen:";
+            // 
+            // scheduleReadout
+            // 
+            this.scheduleReadout.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.scheduleReadout.Location = new System.Drawing.Point(12, 177);
+            this.scheduleReadout.Name = "scheduleReadout";
+            this.scheduleReadout.Size = new System.Drawing.Size(230, 262);
+            this.scheduleReadout.TabIndex = 9;
+            this.scheduleReadout.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Results";
+            // 
+            // exportButton
+            // 
+            this.exportButton.Enabled = false;
+            this.exportButton.Location = new System.Drawing.Point(12, 471);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(230, 75);
+            this.exportButton.TabIndex = 11;
+            this.exportButton.Text = "Export .ics File";
+            this.exportButton.UseVisualStyleBackColor = true;
+            this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
+            // 
+            // tlcTimeShift
+            // 
+            this.tlcTimeShift.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tlcTimeShift.Location = new System.Drawing.Point(121, 445);
+            this.tlcTimeShift.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.tlcTimeShift.Minimum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            -2147483648});
+            this.tlcTimeShift.Name = "tlcTimeShift";
+            this.tlcTimeShift.Size = new System.Drawing.Size(120, 20);
+            this.tlcTimeShift.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 447);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Time Shift (Hours)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1116, 558);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tlcTimeShift);
+            this.Controls.Add(this.exportButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.scheduleReadout);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.scrapeButton);
+            this.Controls.Add(this.BBYpassWord);
+            this.Controls.Add(this.BBYuserName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tlcBrowser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Easy TLC (Beta)";
+            ((System.ComponentModel.ISupportInitialize)(this.tlcTimeShift)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser tlcBrowser;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox BBYuserName;
+        private System.Windows.Forms.TextBox BBYpassWord;
+        private System.Windows.Forms.Button scrapeButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox scheduleReadout;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button exportButton;
+        private System.Windows.Forms.NumericUpDown tlcTimeShift;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ EasyTLC/Form1.cs
@@ -0,0 +1,102 @@
+﻿using DDay.iCal;
+using DDay.iCal.Serialization.iCalendar;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace EasyTLC
+{
+    public partial class Form1 : Form
+    {
+        private List<string> rawShifts;
+        private string rawMonth;
+        private string rawYear;
+        private iCalendar icsResult;
+
+        public Form1()
+        {
+            InitializeComponent();
+            rawShifts = new List<string>();
+            icsResult = new iCalendar();
+        }
+
+        // Turn self off, then place the user/pass and click login
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Plug in the values from the form.
+            tlcBrowser.Document.All["login"].SetAttribute("value", BBYuserName.Text);
+            tlcBrowser.Document.All["password"].SetAttribute("value", BBYpassWord.Text);
+
+            // Find and click the login link
+            foreach (HtmlElement curElement in tlcBrowser.Document.GetElementsByTagName("span"))
+            {
+                if (curElement.GetAttribute("classname").Equals("buttonNavigationText"))
+                    curElement.InvokeMember("click");
+            }
+        }
+
+        // Scape the schedule.
+        private void scrapeButton_Click(object sender, EventArgs e)
+        {
+            // Turn the scrape button off, since clicking it twice under any circumstance will cause sadness.
+            scrapeButton.Enabled = false;
+
+            // Grab current month and year
+            foreach (HtmlElement curElement in tlcBrowser.Document.GetElementsByTagName("span"))
+            {
+                if (curElement.GetAttribute("classname").Equals("calMonthTitle"))
+                    rawMonth = curElement.InnerText;
+
+                if (curElement.GetAttribute("classname").Equals("calYearTitle"))
+                    rawYear = curElement.InnerText;
+            }
+
+            // Grab shifts from days that are either today or in the future.
+            foreach (HtmlElement curElement in tlcBrowser.Document.GetElementsByTagName("td"))
+            {
+                if (curElement.GetAttribute("classname").Equals("calendarCellRegularCurrent") || curElement.GetAttribute("classname").Equals("calendarCellRegularFuture"))
+                {
+                    // We insert a ":00" so that the time is 10:15:00 PM instead of 10:15 PM, this lets us use DateTime.Parse() later.
+                    rawShifts.Add(curElement.InnerText.Substring(0, 13).Replace(System.Environment.NewLine,"").Insert(8,":00"));
+                }
+            }
+
+            // Dump to the readout to the textbox so the user can verify it was scraped correctly.
+            scheduleReadout.Text += rawMonth + " " + rawYear + "\n";
+            foreach (string entry in rawShifts)
+                scheduleReadout.Text += entry + "\n";
+
+            // Now that we've scraped, we are allowed to export.
+            exportButton.Enabled = true;
+        }
+
+        // Create and save our .ics file.
+        private void exportButton_Click(object sender, EventArgs e)
+        {
+            // Make a list of events, setting the output cal to the empty one we made in the contructor.
+            UniqueComponentList<Event> events = new UniqueComponentList<Event>(icsResult);
+
+            // Step through our list of scraped shifts.
+            foreach (string shift in rawShifts)
+            {
+                // Make a new DateTime object, and have it parse the string.
+                // We add in the year and month we scraped earlier, along with some spaces for formatting.
+                // We also adjust the new DateTime by the offset the user chose on the form, for timezone shifting.
+                DateTime datShift = DateTime.Parse(rawYear + " " + rawMonth + " " + shift).AddHours((int)tlcTimeShift.Value);
+
+                // Make a new event, using the DateTime we just magic'd up.
+                var evt = new Event();
+                evt.Summary = "Work"; // Very creative name.
+                evt.Start = new iCalDateTime(datShift);
+                // Eight hour shifts are the norm, so just assume the shift is eight hours.
+                // I know not every shift is, but oh well.
+                evt.Duration = new TimeSpan(8, 0, 0); 
+                events.Add(evt); // Add this newly created event to the events list.  This will make it part of our calendar automagically.
+            }
+ 
+            // Create a serializer and have it do its voodoo.
+            iCalendarSerializer serializer = new iCalendarSerializer(icsResult);
+            serializer.Serialize(icsResult, "schedule.ics");
+        }
+    }
+}
